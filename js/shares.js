116a4531e86c07f1389021d1400b5a92ebdcc6aa
peachl <peachl@outlook.com>
2022-04-13 16:12:35 +0800

    Site updated: 2022-04-13 16:12:32

--- js/shares.js
+++ js/shares.js
@@ -1,6 +1,6 @@
 let shareIcon = document.querySelector('#share-icon')
 let shareContent = document.querySelector('.share-content')
-shareIcon && shareIcon.addEventListener("click", openOrHideShareContent, false)
+shareIcon.addEventListener("click", openOrHideShareContent, false)
 
 function openOrHideShareContent() {
   let isHidden = shareContent.classList.contains('hidden')
